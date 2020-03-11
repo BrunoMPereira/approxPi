81c003596f5de25ed772248bf69d2add519da6c0
bmcapereira <bmca.pereira@campus.fct.unl.pt>
2020-03-11 00:04:10 +0000

    CSV Output to excel

--- C/bash_run_tests.sh
+++ C/bash_run_tests.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 ITEREATIONS=(1000 10000 100000 1000000 10000000 100000000)
 THREADS=(1 2 4 8 16)
 NRUNS=5
-
+echo "exec_time, iterations, nThreads" | tee output_file.csv
 for IT in "${ITEREATIONS[@]}"; do
   for TH in "${THREADS[@]}"; do
     for R in `seq 1 $NRUNS`; do
@@ -9,4 +10,4 @@ for IT in "${ITEREATIONS[@]}"; do
             
     done
   done
-done | tee output_file.csv
+done | tee -a output_file.csv
